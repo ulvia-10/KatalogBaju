01bd70315229e2e95fbed3687080346fabf39c2d
Ulvia Yulianti <ulvia.yulianti@gmail.com>
2020-03-26 06:10:53 +0700

    main layout

--- app/src/main/java/com/ulvia/katalogbaju/MainActivity.java
+++ app/src/main/java/com/ulvia/katalogbaju/MainActivity.java
@@ -2,6 +2,7 @@ package com.ulvia.katalogbaju;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void BackPress(View view) {
+    }
+
+    public void DressPress(View view) {
+    }
+
+    public void BagsPress(View view) {
+    }
+
+    public void ShoesPress(View view) {
     }
 }
