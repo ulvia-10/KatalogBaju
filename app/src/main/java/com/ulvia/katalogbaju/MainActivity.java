6c4035a842e2b757bb1c682d8f41267f77ce407c
Ulvia Yulianti <ulvia.yulianti@gmail.com>
2020-03-26 07:10:22 +0700

    exit back

--- app/src/main/java/com/ulvia/katalogbaju/MainActivity.java
+++ app/src/main/java/com/ulvia/katalogbaju/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ulvia.katalogbaju;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,16 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void BackPress(View view) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     public void DressPress(View view) {
+        Intent intent = new Intent(MainActivity.this, RecycleActivity.class);
+        startActivity(intent);
     }
 
     public void BagsPress(View view) {
+        Intent intent = new Intent(MainActivity.this, RecycleActivity.class);
+        startActivity(intent);
     }
 
     public void ShoesPress(View view) {
+        Intent intent = new Intent(MainActivity.this, RecycleActivity.class);
+        startActivity(intent);
     }
 }
